0ab6bd4e4d2bec4f22828b2b57011698af80f198
kakynchik <ukakyn@gmail.com>
2023-05-18 20:47:32 +0300

    trete zavdannya

--- main.py
+++ main.py
@@ -1,41 +1,13 @@
-#ітератор
-lst = [1,2,3,4,5,6]
-print(iter(lst))
-
-class myiterator:
-    def __init__(self, data):
-        self.data = data
+class ListIterator:
+    def __init__(self, lst):
+        self.lst = lst
         self.index = 0
     def __iter__(self):
         return self
     def __next__(self):
-        if self.index >= len(self.data):
-            raise stopiteration
-        value = self.data[self.index]
-        self.index += 1
-        return value
-for num in myiterator(lst):
-    print(num)
-
-def mygenerator(data):
-    for item in data:
-        yield item
-
-for num mygenerator(lst):
-    print(num)
-
-def tor():
-    def plus(a,b):
-        return a + b
-    def sub (a,b):
-        return a - b
-    def mnoz(a,b):
-        return a * b
-    def dil(a,b):
-        if b != 0:
-            return a / b
+        if self.index >= len(self.lst):
+            raise StopIteration
         else:
-            raise ValueError("pomilka!, delish na nol bratishka")
-    return add, sub, mnoz, dil
-add, sub, mnoz, dil = tor()
-print(dil(3, 0))
+            result = self.lst[self.index]
+            self.index += 1
+            return result
