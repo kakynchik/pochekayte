b2f3a570810c893c624a7cb915afc431e029c848
kakynchik <ukakyn@gmail.com>
2023-05-18 20:44:56 +0300

    druge zavdannya

--- main.py
+++ main.py
@@ -1,41 +1,14 @@
-#ітератор
-lst = [1,2,3,4,5,6]
-print(iter(lst))
+class SquareGenerator
+    def __init__(self, n):
+        if not isinstance(n, (int, float)):
+            raise ValueError("ce povinno bytu 4islom")
+        self.n = n
+    def generator_square(self):
+        for i in range(1, int(self.n) + 1):
+            yield i ** 2
 
-class myiterator:
-    def __init__(self, data):
-        self.data = data
-        self.index = 0
-    def __iter__(self):
-        return self
-    def __next__(self):
-        if self.index >= len(self.data):
-            raise stopiteration
-        value = self.data[self.index]
-        self.index += 1
-        return value
-for num in myiterator(lst):
-    print(num)
-
-def mygenerator(data):
-    for item in data:
-        yield item
-
-for num mygenerator(lst):
-    print(num)
-
-def tor():
-    def plus(a,b):
-        return a + b
-    def sub (a,b):
-        return a - b
-    def mnoz(a,b):
-        return a * b
-    def dil(a,b):
-        if b != 0:
-            return a / b
-        else:
-            raise ValueError("pomilka!, delish na nol bratishka")
-    return add, sub, mnoz, dil
-add, sub, mnoz, dil = tor()
-print(dil(3, 0))
+try:
+    for num in SquareGenerator(20).generator_square():
+        print(num)
+except TypeError as e:
+    print(e)
