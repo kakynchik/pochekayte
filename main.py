d68dad82e97a9fb1e6a22c0e39b1fada2ede05b0
kakynchik <ukakyn@gmail.com>
2023-05-18 20:33:14 +0300

    pershe zavdannya

--- main.py
+++ main.py
@@ -1,41 +1,20 @@
-#ітератор
-lst = [1,2,3,4,5,6]
-print(iter(lst))
-
-class myiterator:
-    def __init__(self, data):
-        self.data = data
-        self.index = 0
+class OddIterator:
+    def __init__(self, n):
+        if not isinstance(n, int) or n <= 0:
+            raise ValueError("treba n cile naturalne chislo")
+        self.n = n
+        self.current = 1
     def __iter__(self):
         return self
     def __next__(self):
-        if self.index >= len(self.data):
-            raise stopiteration
-        value = self.data[self.index]
-        self.index += 1
-        return value
-for num in myiterator(lst):
-    print(num)
-
-def mygenerator(data):
-    for item in data:
-        yield item
-
-for num mygenerator(lst):
-    print(num)
-
-def tor():
-    def plus(a,b):
-        return a + b
-    def sub (a,b):
-        return a - b
-    def mnoz(a,b):
-        return a * b
-    def dil(a,b):
-        if b != 0:
-            return a / b
+        if self.current > self.n:
+            raise StopIteration
         else:
-            raise ValueError("pomilka!, delish na nol bratishka")
-    return add, sub, mnoz, dil
-add, sub, mnoz, dil = tor()
-print(dil(3, 0))
+            result = self.current
+            self.current +=2
+            return result
+try:
+    for num in OddIterator(9):
+        print(num)
+except ValueError as e:
+    print(e)
