df320cf1fd54404a87d21e0ed80ae22868520889
kakynchik <ukakyn@gmail.com>
2023-05-18 19:55:42 +0300

    priklad vikoristannya

--- main.py
+++ main.py
@@ -1,41 +1,11 @@
-#ітератор
-lst = [1,2,3,4,5,6]
-print(iter(lst))
-
-class myiterator:
-    def __init__(self, data):
-        self.data = data
-        self.index = 0
-    def __iter__(self):
-        return self
-    def __next__(self):
-        if self.index >= len(self.data):
-            raise stopiteration
-        value = self.data[self.index]
-        self.index += 1
-        return value
-for num in myiterator(lst):
-    print(num)
-
-def mygenerator(data):
-    for item in data:
-        yield item
-
-for num mygenerator(lst):
-    print(num)
-
-def tor():
-    def plus(a,b):
-        return a + b
-    def sub (a,b):
-        return a - b
-    def mnoz(a,b):
-        return a * b
-    def dil(a,b):
-        if b != 0:
-            return a / b
-        else:
-            raise ValueError("pomilka!, delish na nol bratishka")
-    return add, sub, mnoz, dil
-add, sub, mnoz, dil = tor()
-print(dil(3, 0))
+try:
+    num1 = int(input("Введіть перше число: "))
+    num2 = int(input("Введіть друге число: "))
+    result = num1 / num2
+    print("Результат: ", result)
+except ValueError:
+    print("Введено некоректне число. Будь ласка, введіть число цілочисельного формату.")
+except ZeroDivisionError:
+    print("Ділення на нуль неможливе. Будь ласка, введіть ненульове число для другого числа.")
+except Exception as e:
+    print("Сталася помилка:", str(e))
